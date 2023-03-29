ca20cef8808ced47bf7e3d25e5f245c6f4280ea8
Dingzhou <dw299@duke.edu>
2023-03-29 16:32:30 -0400

    add data to DB

--- SharingLedger/ContentView.swift
+++ SharingLedger/ContentView.swift
@@ -12,11 +12,36 @@ struct ContentView: View {
     
     @ObservedObject var model = ViewModel()
     
+    @State var lastname = ""
+    @State var firstname = ""
+    @State var joinedEventNames = ""
+    
     var body: some View {
-        Text("Hello, world!")
-        List (model.list, id : \.self){ item in
-            Text(item.firstname+" "+item.lastname+" ")
+//        Text("Hello, world!")
+        VStack{
+            List (model.list, id : \.self){ item in
+                Text(item.firstname+" "+item.lastname+" ")
+            }
+            Divider()
+            
+            VStack(spacing: 5){
+                TextField("FirstName", text: $firstname)
+                
+                TextField("LastName", text: $lastname)
+                
+                TextField("joinedEventNames", text: $joinedEventNames)
+                
+                Button(action: {
+                    model.addData(firstname: firstname, lastname: lastname, joinedEventNames: joinedEventNames.components(separatedBy: ","))
+                    lastname = ""
+                    firstname = ""
+                    joinedEventNames = ""
+                }, label: {
+                    Text("Add PersonDeatils")
+                })
+            }
         }
+        .padding()
     }
     
     init(){
