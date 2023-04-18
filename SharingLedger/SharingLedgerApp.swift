e7783d8fbf365542c8cc3086e954db40edd9af14
jz399 <jz399@duke.edu>
2023-04-17 20:14:03 -0400

    Add background refresh.

--- SharingLedger/SharingLedgerApp.swift
+++ SharingLedger/SharingLedgerApp.swift
@@ -23,7 +23,25 @@ struct SharingLedgerApp: App {
                 .environmentObject(storageModel)
                 .onAppear(){
                     print("✅ Start init storageModel from firestore Database")
-                    storageModel.initFromFireStoreDatabase()
+                    
+                    //TODO: Use addSnapshotListener to monitor changes
+                    // Create a timer that fires every 30 seconds and calls your function
+                    let interval = 30
+                    let timer = DispatchSource.makeTimerSource()
+                    timer.schedule(deadline: .now(), repeating: .seconds(interval))
+                    timer.setEventHandler(handler: {
+                        print("✨ Sync StorageModel with firestore database ✨")
+                        storageModel.initFromFireStoreDatabase()
+                    })
+
+                    // Start the timer on a background queue
+                    let queue = DispatchQueue(label: "background refresh", qos: .background)
+                    timer.activate()
+                    timer.setCancelHandler(handler: {
+                        queue.async {
+                            timer.resume()
+                        }
+                    })
                 }
         }
     }
